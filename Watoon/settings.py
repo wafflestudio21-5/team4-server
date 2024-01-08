1953111e17f97d85564254d96bf6e0c78774f279
이수인 <102849003+silee1103@users.noreply.github.com>
2024-01-09 00:32:26 +0900

    Update settings.py

--- Watoon/settings.py
+++ Watoon/settings.py
@@ -17,13 +17,12 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-8yp7ddcg-1jzw42zrg@#_e%$lgk1a2qq!0dhsbf_n2r1u&npn('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -159,8 +158,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "silee11033@gmail.com"
+EMAIL_HOST_PASSWORD = "tzrh vgjj zerv qryl"
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
