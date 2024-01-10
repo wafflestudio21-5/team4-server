3ff3b87008076f01ffb3b7ca44386499da61b004
gs20118 <cyc9820@gmail.com>
2024-01-10 21:30:00 +0900

    view구현 기본 setting

--- Watoon/webtoon/urls.py
+++ Watoon/webtoon/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 
 
 urlpatterns = [
-    
+    #path('api/webtoonList', ), 
+    #path('api/webtoonList/finished', ), 
+    #path('api/webtoonList/recent', ), 
+    #path('api/webtoonList/<std:day>', ), 
+    #path('api/webtoon/<int:pk>', ), 
+    #path('api/webtoon/<int:pk>/episode', ), 
+    #path('api/episode/<int:pk>', ), 
+    #path('api/episode/<int:pk>/comment', ),
+    #path('api/comment/<int:pk>', ), 
+    #path('api/tag/<int:pk>', ),   
 ]
