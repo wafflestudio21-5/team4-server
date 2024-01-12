0f317a6fe318522f42f51e26f24efcf7130334c6
silee1103 <silee1103@snu.ac.kr>
2024-01-13 05:54:23 +0900

    url 컨벤션에 맞게 지정

--- Watoon/settings.py
+++ Watoon/settings.py
@@ -173,8 +173,8 @@ REST_FRAMEWORK = {
     ),
 	'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ],
 }
 
