5701cc95dbed127163db93fe7af6055035a740cd
silee1103 <silee1103@snu.ac.kr>
2024-01-13 12:09:55 +0900

    swagger 추가

--- Watoon/urls.py
+++ Watoon/urls.py
@@ -14,17 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenRefreshView
 from allauth.account.views import ConfirmEmailView, EmailVerificationSentView
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
 from user.views import MyTokenObtainPairView
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="watoon",
+        default_version="v.1",
+        description="watoon API 문서",
+        terms_of_service="https://www.watoon.com/terms/",
+        contact=openapi.Contact(name="test", email="test@test.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
-    
+
     # #JWT
     # path('token/',  MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
@@ -41,5 +59,8 @@ urlpatterns = [
 
     # rest
     path('api-auth/', include('rest_framework.urls')),
+    
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),   
 ]
-
