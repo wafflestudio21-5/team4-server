3c787fedb583d90591aeb612d6018b0521dcd4fd
gs20118 <69315363+gs20118@users.noreply.github.com>
2024-01-29 01:50:07 +0900

    Update 0010_remove_episode_dislikedby_webtoon_titleimage_and_more.py

--- webtoon/migrations/0010_remove_episode_dislikedby_webtoon_titleimage_and_more.py
+++ webtoon/migrations/0010_remove_episode_dislikedby_webtoon_titleimage_and_more.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='episode',
-            name='dislikedBy',
-        ),
         migrations.AlterField(
             model_name='rating',
             name='rating',
