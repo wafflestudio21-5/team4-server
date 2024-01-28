7c4bd0c4d8e6fae86ea1495c0f5757c50ee45fa6
gs20118 <cyc9820@gmail.com>
2024-01-28 19:37:13 +0900

    model modified

--- webtoon/models.py
+++ webtoon/models.py
@@ -30,8 +30,8 @@ class DayOfWeek(models.Model):
 class Webtoon(models.Model):
     """웹툰 모델"""
     title = models.CharField(max_length=50)
-    titleImage = models.CharField(max_length=50)
-    description = models.CharField(max_length=200, null=True)
+    titleImage = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=200)
     isFinished = models.BooleanField(default=False)
     totalRating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
 
@@ -76,7 +76,7 @@ class Episode(models.Model):
     def __str__(self):
         return str(self.episodeNumber) + '. ' + self.title
 
-        
+
 
 
 class Comment(models.Model):
