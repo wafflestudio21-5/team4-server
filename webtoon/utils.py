c1b6df76d9c82cb3e3eda47b2fe59fd45a7d622d
gs20118 <cyc9820@gamil.com>
2024-02-02 21:43:36 +0900

    image upload url modified

--- webtoon/utils.py
+++ webtoon/utils.py
@@ -1,8 +1,8 @@
 def image_upload_path(instance, filename):
-    return f'img/episode/{instance.episode.id}/{filename}'
+    return f'img/episode/{instance.episode.pk}/{filename}'
 
 def titleImage_upload_path(instance, filename):
-    return f'titleImage/{instance.id}/{filename}'
+    return f'titleImage/{instance.pk}/{filename}'
 
 def thumbnail_upload_path(instance, filename):
-    return f'thumbnail/{instance.id}/{filename}'
+    return f'thumbnail/{instance.pk}/{filename}'
