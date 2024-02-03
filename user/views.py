26a0e91f9316cb58deb143150b5a9b5a334d318c
JUNG SIHUN <gssihunjung@gmail.com>
2024-02-03 16:53:13 +0900

    Update views.py

--- user/views.py
+++ user/views.py
@@ -289,6 +289,7 @@ class GoogleLogin(SocialLoginView):
 
 class NicknameChangeView(APIView):
     def post(self, request):
+        nickname = ""
         nickname = request.data['nickname']
         if User.objects.filter(nickname=nickname).exists():
             return Response(status=400)
